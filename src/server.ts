3fad27923f6aa86837122276d65ec6e6002135c4
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-14 07:03:23 +0600

    use middleware and type and post route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// middleware
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
 app.get("/user", (req, res) => {
-  res.send("Hello user");
+  res.status(200).json({
+    message: "Hello developer",
+    author: "Programming hero",
+  });
+});
+
+// post user data
+app.post("/", async (req: Request, res: Response) => {
+  console.log("Hello user data  and request:", req.body);
 });
 
 app.listen(port, () => {
