11e2254b04a1caf44c7a29693e31fbef4a124760
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-14 11:04:52 +0600

    create table and write query and  connect data with neon

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,44 @@ import express, {
   type Request,
   type Response,
 } from "express";
+// import pg admin
+import { Pool } from "pg";
 const app: Application = express();
 const port = 3000;
 
-// middleware
+//* middleware
 app.use(express.json());
 
+//* create pool express to postgressql
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_r5NlXadOIKn3@ep-nameless-forest-abja3kpt-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+//* init db
+const initBD = async () => {
+  try {
+    await pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT true,
+    age INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
+    console.log("database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initBD();
+
+//* routeing system
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
@@ -22,7 +54,15 @@ app.get("/user", (req, res) => {
 
 // post user data
 app.post("/", async (req: Request, res: Response) => {
-  console.log("Hello user data  and request:", req.body);
+  // console.log("Hello user data  and request:", req.body);
+  const { name, price, password } = req.body;
+  res.status(200).json({
+    message: "Created",
+    data: {
+      name,
+      price,
+    },
+  });
 });
 
 app.listen(port, () => {
